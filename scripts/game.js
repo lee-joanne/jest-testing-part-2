15be1e7a450624cb90cdca39606f408182610709
Joanne <lee.joanne54@gmail.com>
2022-04-26 21:01:26 +0000

    initial commit

---
+++ scripts/game.js
@@ -0,0 +1,36 @@
+/**
+ * JavaScript Strucutre:
+ * game{object}
+ * newGame()
+ * addTurn()
+ * showTurns()
+ * lightsOn()
+ * playerTurn()
+ * showScore()
+ */
+
+// THIS IS AN OBJECT
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ['button1', 'button2', 'button3', 'button4']
+};
+
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    document.getElementById('score')
+    showScore();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score
+}
+
+module.exports = {
+    game,
+    newGame,
+    showScore
+};
